079f9a3693fc2654a34099c3fb15140ac453745f
PathumLD <pathumlk10158@gmail.com>
2025-03-25 01:52:51 +0530

    Restore vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Important for proper routing
   plugins: [react()],
-  // other configurations
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
